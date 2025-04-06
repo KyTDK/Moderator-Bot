fe5de5e92e296647741533425cee90c726d0fbf7
Kyan Taber <taberkyan@gmail.com>
2025-04-06 19:54:25 +1000

    Update user count calculation to exclude bot accounts in on_ready event

--- bot.py
+++ bot.py
@@ -21,8 +21,8 @@ async def on_ready():
     #     bot.tree.clear_commands(guild=guild)
     #     await bot.tree.sync(guild=guild)
     # await bot.tree.sync()
-    total_users = sum(guild.member_count for guild in bot.guilds)
-    print(f"Connected to {len(bot.guilds)} guilds with a total of {total_users} users.")
+    total_users_not_bots = sum(len([member for member in guild.members if not member.bot]) for guild in bot.guilds)
+    print(f"Connected to {len(bot.guilds)} guilds with a total of {total_users_not_bots} users.")
 
 @bot.event
 async def setup_hook():
