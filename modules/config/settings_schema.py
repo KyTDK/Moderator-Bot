51b86dff4cedccc9eec17dccac01a573bf1ee374
Kyan Taber <taberkyan@gmail.com>
2025-05-02 01:56:07 +1000

    Rename setting for unmuting users on safe profile picture change

--- modules/config/settings_schema.py
+++ modules/config/settings_schema.py
@@ -134,7 +134,7 @@ SETTINGS_SCHEMA = {
         default="Your profile picture was detected to contain explicit content"
     ),
     "unmute-on-safe-pfp": Setting(
-        name="nsfw-pfp-message",
+        name="unmute-on-safe-pfp",
         description="Remove timeout of a user once they have changed their profile picture to something appropriate.",
         setting_type=bool,
         default=False
