2c8da7677e3555faa00e82778529207dd067b4ad
Your Name <you@example.com>
2025-07-04 19:40:37 +1000

    fix: remove redundant OpenCV logging configuration for improved clarity

--- modules/detection/nsfw.py
+++ modules/detection/nsfw.py
@@ -1,6 +1,7 @@
 import os
 import traceback
 import asyncio
+import cv2
 import filetype
 import uuid
 import aiohttp
@@ -24,15 +25,6 @@ from tempfile import NamedTemporaryFile, gettempdir
 from PIL import Image, ImageSequence
 from modules.utils import clip_vectors
 import pillow_avif
-
-os.environ["OPENCV_LOG_LEVEL"] = "ERROR"
-import cv2
-
-# Silence as imreadanimation runs all files, including not supported formats, this doesn't break anything, but we do this to ensure all possible animations are processed
-if hasattr(cv2, "utils") and hasattr(cv2.utils, "logging"):
-    cv2.utils.logging.setLogLevel(cv2.utils.logging.LOG_LEVEL_ERROR)
-else:
-    os.environ["OPENCV_LOG_LEVEL"]="ERROR"
 
 TMP_DIR = os.path.join(gettempdir(), "modbot")
 os.makedirs(TMP_DIR, exist_ok=True)
