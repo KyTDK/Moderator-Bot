b95a7225c9c595b9cfc9436827ab8fa40937c680
KyTDK <71435411+KyTDK@users.noreply.github.com>
2025-08-08 15:59:37 +1000

    fix: use proper name

--- cogs/dashboard.py
+++ cogs/dashboard.py
@@ -7,7 +7,7 @@ class DashboardCog(commands.Cog):
 
     @app_commands.command(name="dashboard", description="Open the dashboard for this server.")
     @app_commands.guild_only()
-    @app_commands.default_permissions(admin=True)
+    @app_commands.default_permissions(administraor=True)
     async def dashboard(self, interaction: Interaction):
         """Open the dashboard for this server."""
         guild_id = interaction.guild.id
