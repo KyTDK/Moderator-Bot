1885a4b0f89c6b5618b8c6b3dbd19ffdad8ab279
KyTDK <71435411+KyTDK@users.noreply.github.com>
2025-08-11 13:57:16 +1000

    modify upper and lower bound for max workers in free queue

--- cogs/aggregated_moderation.py
+++ cogs/aggregated_moderation.py
@@ -26,9 +26,9 @@ class AggregatedModerationCog(commands.Cog):
         # Adjust queue size based on backlog
         backlog = queue.queue.qsize()
         if not accelerated:
-            if backlog > 13 and queue.max_workers < 2:
+            if backlog > 16 and queue.max_workers < 2:
                 await queue.resize_workers(2)
-            elif backlog <= 7 and queue.max_workers > 1:
+            elif backlog <= 10 and queue.max_workers > 1:
                 await queue.resize_workers(1)
 
     async def handle_message(self, message: discord.Message):
