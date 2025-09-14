f5e59854784f0815fc451a461a63cbc076ab8f3f
KyTDK <71435411+KyTDK@users.noreply.github.com>
2025-09-14 13:07:28 +1000

    decrease clip threshold

--- modules/nsfw_scanner/constants.py
+++ modules/nsfw_scanner/constants.py
@@ -11,7 +11,7 @@ LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", 0))
 TMP_DIR = os.path.join(gettempdir(), "modbot")
 os.makedirs(TMP_DIR, exist_ok=True)
 
-CLIP_THRESHOLD = 0.80  # Threshold for similarity search
+CLIP_THRESHOLD = 0.78  # Threshold for similarity search
 HIGH_ACCURACY_SIMILARITY = 0.90  # Min similarity to skip API when high-accuracy is enabled
 MAX_FRAMES_PER_VIDEO = 5
 ACCELERATED_MAX_FRAMES_PER_VIDEO = 100
