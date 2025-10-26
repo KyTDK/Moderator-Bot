9df27f11c6fd621a1d0443d918cc7d0f4d5f9b27
KyTDK <71435411+KyTDK@users.noreply.github.com>
2025-10-26 19:52:35 +1100

    fix

--- cogs/aggregated_moderation.py
+++ cogs/aggregated_moderation.py
@@ -1,6 +1,6 @@
 
 from discord.ext import commands
-from cogs.aggregated_moderation.cog import setup
+from cogs.aggregated_moderation.cog import setup as setup_cog
 
 async def setup(bot: commands.Bot):
-    await setup(bot)
+    await setup_cog(bot)
